d226abcc380c0695292d6570a07fd1220a72a12a
p4mp3rs <128310751+p4mp3rs@users.noreply.github.com>
2023-04-02 18:58:24 +0300

    Add files via upload
    window WORKS

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 from pygame import *
+
+window = display.set_mode((700,500))
+
+game = True
+clock = time.Clock()
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    window.fill((255,255,255))
+
+    display.update()
+    clock.tick(60)

d4cc6334a59e10afb611606aab47fd255a802de2
p4mp3rs <128310751+p4mp3rs@users.noreply.github.com>
2023-04-09 19:04:51 +0300

    Add files via upload
    update

--- main.py
+++ main.py
@@ -5,17 +5,25 @@ window = display.set_mode((700,500))
 game = True
 clock = time.Clock()
 
-btn1 = Button('start.png', 100,100,100,50)
-btn2 = Button('exit.png', 100,150,100,50)
+btn1 = Button('start.png', 300,150,100,50)
+btn2 = Button('exit.png', 300,200,100,50)
+
+run = False
+
+
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+        if e.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                run - False
 
-    window.fill((255,255,255))
+
+    window.fill((255,255,255))   
     if btn1.draw(window):
         print('Pressed')
     if btn2.draw(window):
-        print('Pressed')
+        game = False
     display.update()
     clock.tick(60)

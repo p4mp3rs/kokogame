f75f360010a51e142d46dd7c23ebd6d89c39747f
БОТ <128310751+p4mp3rs@users.noreply.github.com>
2023-05-07 17:57:16 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pygame import *
 from button import Button
-from sprite import Player,GameSprite
+from sprite import Player,Egg,GameSprite
+from random import randint
 window = display.set_mode((700,500))
 
 background_image = image.load('bg.png')
@@ -11,9 +12,17 @@ clock = time.Clock()
 btn1 = Button('start.png', 300,150,100,50)
 btn2 = Button('exit.png', 300,200,100,50)
 
+eggs = []
 run = False
-hen = Player('kokohen.png',345,400,45,45)
+hen = Player('kokohen.png',345,400,45,45,5)
+for i in range(6):
+    egg = Egg('kokoegg.png',randint(80,650),randint(0,80),30,30,randint(1,2))
+    eggs.append(egg)
 
+font.init() 
+font = font.SysFont("Arial",20)
+score = 0
+text = font.render('Score:'+str(score),True,(255,212,57))
 while game:
     for e in event.get():
         if e.type == QUIT:
@@ -22,14 +31,22 @@ while game:
             if e.key == K_ESCAPE:
                 run = False
     
-    
+
 
     if run:
         window.blit(background_image, (0,0)) 
         hen.draw(window)
         hen.move()
+        for e in eggs:
+            e.update(window)
+        
+
+        
+        window.blit(text,(20,20))
+        
+
+        
     else:
-      
         if btn1.draw(window):   
             run = True   
         if btn2.draw(window):
--- sprite.py
+++ sprite.py
@@ -1,12 +1,16 @@
 from pygame import *
-
+from random import randint
 class GameSprite(sprite.Sprite):
-    def __init__(self,image_name, x, y, width, height):
+    def __init__(self,image_name, x, y, width, height, speed):
+
+        self.image1 = transform.scale(image.load('kokohen.png'),(width, height))
         self.image = transform.scale(image.load(image_name),(width, height))
-        self.rect = self.image.get_rect()
+
+        self.image2 = transform.scale(image.load("kokohen_2.png"),(width, height))
+        self.rect = self.image1.get_rect()
         self.rect.x = x
         self.rect.y = y
-    
+        self.speed = speed
     def draw(self,window):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
@@ -14,13 +18,23 @@ class Player(GameSprite):
 
     def move(self):
         keys = key.get_pressed()
-        if key == [K_LEFT] and self.rect.x != 5:
-            self.rect.x -= 5
-        elif key == [K_RIGHT] and self.rect.x != 695:
-            self.rect.x += 5
+        if keys[K_LEFT] == True and self.rect.x != 5:
+            self.rect.x = self.rect.x - self.speed  
+            self.image = self.image1
+        elif keys[K_RIGHT] == True and self.rect.x != 645:
+            self.rect.x = self.rect.x + self.speed
+            self.image = self.image2
 
 class Egg(GameSprite):
-    pass
+    def update(self, window):
+        self.rect.y = self.rect.y + self.speed
+        global lost
+        if self.rect.y > 500:
+            self.rect.y = randint(0,80)
+            self.rect.x = randint(50,650)
+            self.speed = randint(1,2)    
+        window.blit(self.image,(self.rect.x,self.rect.y))
+    
     
 
 
